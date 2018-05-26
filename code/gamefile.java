541e77fa675be90101b5580d981a78f0a494e7ac
Mostafa Rizk <m.rizk.student@gmail.com>
2018-05-26 09:37:10 -0600

    Added scanner and introductory message

--- code/gamefile.java
+++ code/gamefile.java
@@ -1,5 +1,11 @@
+import java.io.*;
+import java.util.*;
+
 public class gamefile {
    public static void main(String args[]) {
+      Scanner kbReader = new Scanner(System.in);
+      char userIcon;
+      System.out.println("Welcome to Tic-tac-toe. You will choose where to put your piece using letters and numbers to represent a grid system where the top left is the origin. For example, type 'A2' for first row and second column.");
       
    }
 }
