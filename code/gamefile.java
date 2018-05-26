787c832abb06c311923672def642d3f642be50a5
Mostafa Rizk <m.rizk.student@gmail.com>
2018-05-26 02:12:14 -0600

    Created characters

--- code/gamefile.java
+++ code/gamefile.java
@@ -1,6 +1,12 @@
 public class gamefile {
    public static void main(String args[]) {
-      
+      character Player = new character();
+      character Christian = new character();
+      Christian.name = "Christian Shawcross"; Christian.gender = "Male";
+      character Aeden = new character();
+      Aeden.name = "Aeden Stewart"; Aeden.gender = "Male";
+      character Robin = new character();
+      Robin.name = "Robin Melun"; Robin.gender = "Female";
    }
    public static class character {
       String name;
